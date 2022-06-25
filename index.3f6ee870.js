710dd404c3ecb1dfb6a7453b9cceb8c639f5c4c8
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-25 11:07:14 -0500

    Updates

---
+++ index.3f6ee870.js
@@ -0,0 +1,1 @@
+const modal=document.querySelector(".modal"),btnSpiderman=document.querySelector(".btn-spiderman"),btnClose=document.querySelector(".btn-close");btnSpiderman.addEventListener("click",(()=>{modal.classList.remove("hidden"),modal.classList.add("visible")})),btnClose.addEventListener("click",(()=>{modal.classList.add("hidden"),modal.classList.remove("visible")}));
